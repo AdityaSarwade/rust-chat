913463d658f4d6eaf289826e33c32dd5fb5cda85
Aditya Sarwade <adityasarwade7@gmail.com>
2024-06-02 23:55:26 +0530

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+#[macro_use]
+extern crate rocket;
+
+use rocket::{
+    form::Form,
+    fs::{relative, FileServer},
+    response::stream::{Event, EventStream},
+    serde::{Deserialize, Serialize},
+    tokio::{
+        select,
+        sync::broadcast::{channel, error::RecvError, Sender},
+    },
+    Shutdown, State,
+};
+
+#[get("/health-check")]
+fn health_check() -> &'static str {
+    "Server is Online."
+}
+
+#[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Message {
+    #[field(validate=len(..30))]
+    pub room: String,
+    #[field(validate=len(..20))]
+    pub username: String,
+    pub message: String,
+}
+
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    // A send fails if there are no active subscribers. This is ok.
+    let _res = queue.send(form.into_inner());
+}
+
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg{
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+            yield Event::json(&msg);
+        }
+    }
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(channel::<Message>(1024).0)
+        .mount("/", routes![health_check, post, events])
+        .mount("/", FileServer::from(relative!("static")))
+}
